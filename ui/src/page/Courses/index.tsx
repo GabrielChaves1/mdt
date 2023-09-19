7a3337b3245d802f539c91b8cc6cbc19da2dc02e
droyendev <gabrielclcplays@gmail.com>
2023-09-19 18:32:00 -0300

    init: courses page

---
+++ ui/src/page/Courses/index.tsx
@@ -0,0 +1,14 @@
+import Banner from '@/components/Banner';
+import * as S from './styles';
+
+export default function RolesHierarchy() {
+  return (
+    <S.Wrapper>
+      <Banner.Root>
+        <Banner.Header>
+          <Banner.Title>Cursos Disponíveis</Banner.Title>
+        </Banner.Header>
+      </Banner.Root>
+    </S.Wrapper>
+  )
+}
