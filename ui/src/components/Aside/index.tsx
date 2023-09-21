5e4dfd292363c520f9b24dde61bb1b744af0edae
droyendev <gabrielclcplays@gmail.com>
2023-09-20 21:57:02 -0300

    refactor: aside component

--- ui/src/components/Aside/index.tsx
+++ ui/src/components/Aside/index.tsx
@@ -1,58 +1,66 @@
 import Button from '../Button';
 import * as S from './styles';
 import { useLocation, useNavigate } from 'react-router-dom';
-import { FileText, Settings, StickyNote, Tv2, User } from 'lucide-react';
+import { BookOpen, Crown, Settings, StickyNote, Tv2, Users } from 'lucide-react';
 import { memo } from 'react';
-
 
 function Aside() {
   const navigate = useNavigate();
   const location = useLocation();
+
+  const paths = [
+    {
+      url: "/",
+      icon: Tv2,
+      text: "Página Inicial"
+    },
+    {
+      url: "/arrest",
+      icon: StickyNote,
+      text: "Prisões"
+    },
+    {
+      url: "/user",
+      icon: Users,
+      text: "Consultar Cidadão"
+    },
+    {
+      url: "/courses",
+      icon: BookOpen,
+      text: "Cursos"
+    },
+    {
+      url: "/roles-hierarchy",
+      icon: Crown,
+      text: "Hierarquia de Cargos"
+    },
+    {
+      url: "/config",
+      icon: Settings,
+      text: "Configurações"
+    },
+  ]
+
+  const handleNavigate = (url: string) => {
+    if(location.pathname == url) return;
+    navigate(url);
+  }
 
   return (
     <S.Container>
       <S.NavigationArea>
         <S.NavigationAreaTitle>Navegação</S.NavigationAreaTitle>
         <S.NavigationAreaContent>
-          <Button 
-            variant={location.pathname == "/" ? 'primary' : 'secondary'} 
-            onClick={() => navigate('/')} 
-            fill>
-            <Tv2 size={18}/>
-            Página Inicial
-          </Button>
-          
-          <Button 
-            variant={location.pathname == "/arrest" ? 'primary' : 'secondary'} 
-            onClick={() => navigate('/arrest')} 
-            fill>
-            <StickyNote size={18}/>
-            Registro de prisão
-          </Button>
-          
-          <Button 
-            variant={location.pathname == "/user" ? 'primary' : 'secondary'} 
-            onClick={() => navigate('/user')} 
-            fill>
-            <User size={18} />
-            Buscar Cidadão
-          </Button>
-
-          <Button 
-            variant={location.pathname == "/roles-hierarchy" ? 'primary' : 'secondary'} 
-            onClick={() => navigate('/roles-hierarchy')} 
-            fill>
-            <FileText size={18} />
-            Hierarquia de Cargos
-          </Button>
-          
-          <Button 
-            variant={location.pathname == "/config" ? 'primary' : 'secondary'} 
-            onClick={() => navigate('/config')} 
-            fill>
-            <Settings size={18} />
-            Configurações
-          </Button>
+          {paths.map(({ url, text, icon: Icon }) => (
+            <Button 
+              key={url}
+              variant={location.pathname == url ? 'primary' : 'secondary'} 
+              onClick={() => handleNavigate(url)} 
+              fill>
+              <Icon size={18}/>
+              {text}
+            </Button>
+          ))}
         </S.NavigationAreaContent>
       </S.NavigationArea>
     </S.Container>
