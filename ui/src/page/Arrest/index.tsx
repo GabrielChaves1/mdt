c2b1085f97d7434742e88f8be9a923b8b28c2c28
droyendev <gabrielclcplays@gmail.com>
2023-09-20 22:01:22 -0300

    refactor: table component structure use

--- ui/src/page/Arrest/index.tsx
+++ ui/src/page/Arrest/index.tsx
@@ -1,27 +1,148 @@
 import Banner from '@/components/Banner';
 import * as S from './styles';
-
-import ViolatorForm from './components/ViolatorForm';
-import PolicersForm from './components/PolicersForm';
-import PrisonDetails from './components/PrisonDetails';
-import CrimeSelector from './components/CrimeSelector';
+import Table from '@/components/Table';
+import { Eye } from 'lucide-react';
+import Action from '@/components/Action';
+import Pagination from '@/components/Pagination';
+import usePaginate from '@/hooks/usePaginate';
 
 export default function Arrest() {
+  const { amountOfPages, currentPage, items, totalOfItems, viewedItems, paginate } = usePaginate(12, 1, [
+    {
+      id: 1,
+      name: "John doe",
+      time: 15,
+      fine: 15000,
+    },
+    {
+      id: 2,
+      name: "John doe",
+      time: 15,
+      fine: 15000,
+    },
+    {
+      id: 3,
+      name: "John doe",
+      time: 15,
+      fine: 15000,
+    },
+    {
+      id: 4,
+      name: "John doe",
+      time: 15,
+      fine: 15000,
+    },
+    {
+      id: 5,
+      name: "John doe",
+      time: 15,
+      fine: 15000,
+    },
+    {
+      id: 6,
+      name: "John doe",
+      time: 15,
+      fine: 15000,
+    },
+    {
+      id: 7,
+      name: "John doe",
+      time: 15,
+      fine: 15000,
+    },
+    {
+      id: 8,
+      name: "John doe",
+      time: 15,
+      fine: 15000,
+    },
+    {
+      id: 9,
+      name: "John doe",
+      time: 15,
+      fine: 15000,
+    },
+    {
+      id: 10,
+      name: "John doe",
+      time: 15,
+      fine: 15000,
+    },
+    {
+      id: 11,
+      name: "John doe",
+      time: 15,
+      fine: 15000,
+    },
+    {
+      id: 12,
+      name: "John doe",
+      time: 15,
+      fine: 15000,
+    },
+    {
+      id: 13,
+      name: "John doe",
+      time: 15,
+      fine: 15000,
+    },
+    {
+      id: 14,
+      name: "John doe",
+      time: 15,
+      fine: 15000,
+    },
+    {
+      id: 15,
+      name: "John doe",
+      time: 15,
+      fine: 15000,
+    },
+    {
+      id: 16,
+      name: "John doe",
+      time: 15,
+      fine: 15000,
+    },
+    {
+      id: 17,
+      name: "John doe",
+      time: 15,
+      fine: 15000,
+    },
+  ]);
+
   return (
     <S.Wrapper>
       <Banner.Root>
         <Banner.Header>
-          <Banner.Title>Registrar prisão</Banner.Title>
+          <Banner.Title>Últimas prisões</Banner.Title>
         </Banner.Header>
-        <Banner.Action>Confirmar</Banner.Action>
+        <Banner.Action>Nova Prisão</Banner.Action>
       </Banner.Root>
 
-      <S.Grid>
-        <ViolatorForm />
-        <PolicersForm />
-        <PrisonDetails />
-        <CrimeSelector />
-      </S.Grid>
+      <S.TableArea>
+        <Table.Root headColumns={["ID", "Prisioneiro", "Tempo", "Multa", "Ações"]}>
+          {items.map((item) => (
+            <Table.Row>
+              <Table.Item>#{item.id}</Table.Item>
+              <Table.Item>{item.name}</Table.Item>
+              <Table.Item>{item.time} meses</Table.Item>
+              <Table.Item>R$ {item.fine.toLocaleString('pt-br')}</Table.Item>
+              <Table.Item>
+                <Action icon={Eye} size='sm' label='Ver detalhes' />
+              </Table.Item>
+            </Table.Row>
+          ))}
+        </Table.Root>
+
+        <Pagination 
+          amountOfPages={amountOfPages}
+          currentPage={currentPage} 
+          itemsBeingViewed={viewedItems}
+          totalOfItems={totalOfItems}
+          onPaginate={(page: number) => paginate(page)}/>
+      </S.TableArea>
     </S.Wrapper>
   )
 }
--- ui/src/page/Arrest/styles.ts
+++ ui/src/page/Arrest/styles.ts
@@ -7,14 +7,9 @@ export const Wrapper = styled(Container)`
   gap: 2rem;
 `
 
-export const Grid = styled.div`
-  display: grid;
+export const TableArea = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: relative;
   flex: 1;
-  grid-template-columns: 44.2rem 55.9rem;
-  grid-template-rows: auto auto 32.9rem;
-  grid-template-areas:
-  'violator policers'
-  'violator crimeSelector'
-  'prisonDetails crimeSelector';
-  gap: 2rem;
 `
