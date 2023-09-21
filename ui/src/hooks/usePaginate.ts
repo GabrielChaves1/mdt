b39be5c6f75bd64aa870e605def8a20851273d6b
droyendev <gabrielclcplays@gmail.com>
2023-09-20 22:00:40 -0300

    add: usePaginate hook to manage pages in table

---
+++ ui/src/hooks/usePaginate.ts
@@ -0,0 +1,13 @@
+import { useState } from "react"
+
+export default function usePaginate<T>(itemsPerPage: number, initialPage = 1, items: Array<T>) {
+  const [currentPage, setCurrentPage] = useState(initialPage)
+  const paginate = (page: number) => setCurrentPage(page)
+  
+  const amountOfPages = Math.ceil(items.length / itemsPerPage);
+  const totalOfItems = items.length;
+  const slicedItems = items.slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage);
+  const viewedItems = (itemsPerPage * currentPage) - (itemsPerPage - slicedItems.length);
+
+  return { paginate, currentPage, amountOfPages, viewedItems, totalOfItems, items: slicedItems }
+}
